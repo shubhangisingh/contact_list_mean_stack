1ac20bccdbe199bd25fa771454baaa7643286a59
Shubhangi <shubhangi.mca09@gmail.com>
2016-08-08 20:44:57 +0530

    first commit

---
+++ public/controllers/controller.js
@@ -0,0 +1,27 @@
+var app = angular.module('myApp', []);
+app.controller('AppCtrl', [ '$scope', '$http', function($scope, $http) {
+    console.log('hello from angu controller')
+
+    var refresh = function () {
+      $http.get('/contactlist').success(function(response){
+        console.log('i got data')
+        $scope.contactlist = response
+        $scope.contact = '';
+      })
+    }
+    refresh();
+    $scope.addcontact = function () {
+      console.log($scope.contact);
+      $http.post('/contactlist', $scope.contact).success(function (response) {
+        console.log(response);
+        refresh();
+      })
+    }
+    $scope.removecontact= function (id) {
+      console.log(id);
+      $http.delete('/contactlist/' + id).success(function (response) {
+        refresh();
+      })
+    }
+
+}]);
---
+++ server.js
@@ -0,0 +1,51 @@
+var express = require('express');
+var app = express();
+var mongojs = require('mongojs');
+var db= mongojs('contactlist', ['contactlist'])
+var bodyParser= require('body-parser')
+
+// app.get('/',function (req, res) {
+//   res.send('Hello from server.js')
+// });
+app.use(express.static(__dirname + '/public'))
+app.use(bodyParser.json())
+app.get("/contactlist", function(req,res){
+  console.log('get get get !!!');
+  // fetch data from mongo
+  db.contactlist.find(function(err, docs){
+    console.log(docs);
+    res.json(docs)
+  })
+  app.post('/contactlist', function (req, res) {
+    console.log(req.body);
+    db.contactlist.insert(req.body, function (err, doc) {
+      res.json(doc)
+
+    })
+  })
+
+  app.delete('/contactlist/:id', function (req, res) {
+    var id= req.params.id;
+    console.log(id);
+    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
+       res.json(doc)
+    })
+  })
+
+  app.get('/contactlist/:id', function(req, res){
+    var id= req.params.id;
+    console.log(id);
+    db.contactlist.find({_id: mongojs.ObjectId(id)}, function (err, doc){
+      res.json(doc)
+    })
+  })
+  app.put('/contactlist/:id', function(req, res){
+    var id= req.params.id;
+    console.log(req.body.name);
+    
+  })
+
+})
+
+app.listen(3000);
+console.log('server running on port 3000');
